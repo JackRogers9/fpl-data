68d4ea305b58cf56cf4b3dad959b6308aa322fc3
Jack Rogers <jack.rogers@iress.com>
2022-08-06 22:28:15 +0100

    chore: Added template GraphQL server

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+        ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+});
+
+server.listen(9000).then(({ url }) => {
+    console.log(`Server is running at ${url}`);
+});
